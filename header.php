99578660c32457d495fcd1f88778a0c82d194f73
abarson <abarson@uvm.edu>
2014-10-20 11:59:39 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 +
 +<!-- ######################     Page header   ############################## -->
 +<header>
-+    <h1>main title for SITE</h1>
++    <h1>Together, we can make the world a better place!</h1>
 +</header>
