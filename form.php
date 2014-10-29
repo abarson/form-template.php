ff73091b628bcdd372db95649fa1425efb9575da
abarson <abarson@uvm.edu>
2014-10-29 11:56:16 -0400

    Update form.php

--- form.php
+++ form.php
@@ -39,18 +39,18 @@ $email = "abarson@uvm.edu";
 //
 // SECTION: 1d form error flags
 //
-//
-//
+//Initialize Error Flags one for each form element we validate
+// in the order they appear in section 1c.
 
-
+$emailERROR = false;
 
 //%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%
 //
 // SECTION: 1e misc variables
 //
 // 
-
-
+// create array to hold error messages filled (if any) in 2d displayed in 3c.
+$errorMsg = array();
 
 
 
@@ -74,14 +74,14 @@ if (isset($_POST["btnSubmit"])) {
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     //
     // SECTION: 2b Sanitize (clean) data 
-    // 
-    // 
+    // remove any potential JavaScript or html code from users input on the
+    // form. Note it is best to follow the same order as declared in section 1c.
 
     
     
 
 
-    
+    $email = filter_var($_POST["txtEmail"], FILTER_SANITIZE_EMAIL);
     
 
 
@@ -89,7 +89,12 @@ if (isset($_POST["btnSubmit"])) {
     //
     // SECTION: 2c Validation
     //
-    // 
+    // Validation section. Check each value for possible errors, empty or
+    // not what we expect. You will need an IF block for each element you will
+    // check (see above section 1c and 1d). The if blocks should also be in the
+    // order that the elements appear on your form so that the error messages
+    // will be in the order they appear. errorMsg will be displayed on the form
+    // see section 3b. The error flag ($emailERROR) will be used in section 3c.
     // 
     // 
     //  
@@ -99,30 +104,25 @@ if (isset($_POST["btnSubmit"])) {
     
         
         
-    
+    if ($email == "") {
+        $errorMsg[] = "Please enter your email address";
+        $emailERROR = true;
+    } elseif (!verifyEmail($email)) {
+        $errorMsg[] = "Your email address appears to be incorrect.";
+        $emailERROR = true;
+    }
         
         
-    
-
-    
-        
-        
-    
-        
-        
-    
-
-
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     //
     // SECTION: 2d Process Form - Passed Validation
     //
-    // 
+    // Process for when the form passes validation (the errorMsg array is empty)
     //
-    
+    if (!$errorMsg) {
+        if ($debug)
+            print "<p>Form is valid</p>";
         
-            
-
         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         //
         // SECTION: 2e Save Data
@@ -186,7 +186,7 @@ if (isset($_POST["btnSubmit"])) {
 
         
         
-    
+    } // end form is valid
     
 } // ends if form was submitted.
 
@@ -201,17 +201,21 @@ if (isset($_POST["btnSubmit"])) {
 
     <?php
     //####################################
+    SECTION 3a.
     //
-    // SECTION 3a.
+    // 
+    // 
     //
-    // 
-    // 
-    // 
-    // 
-    // 
+    // If its the first time coming to the form or there are errors we are going
+    // to display the form.
+    if (isset($_POST["btnSubmit"]) AND empty($errorMsg)) { // closing of if marked with: end body submit
+        print "<h1>Your Request has ";
+    
+    
+    
     
         
-
+     print "been processed</h1>";
         
             
         
@@ -222,50 +226,45 @@ if (isset($_POST["btnSubmit"])) {
         
             
         
+       } else {
         
         
-        
-
-        
-     
-
-
         //####################################
         //
         // SECTION 3b Error Messages
         //
-        // 
+        // display any error messages before we print out the form
 
-        
+       if ($errorMsg) {
+            print '<div id="errors">';
+            print "<ol>\n";
+            foreach ($errorMsg as $err) {
+                print "<li>" . $err . "</li>\n";
+            }
+            print "</ol>\n";
+            print '</div>';
+        } 
             
             
-            
-                
-            
-            
-            
-        
-
-
         //####################################
         //
         // SECTION 3c html Form
         //
         /* Display the HTML form. note that the action is to this same page. $phpSelf
           is defined in top.php
-          
+          NOTE the line:
 
-          
+          value="<?php print $email; ?>
 
-          
-          
+          this makes the form sticky by displaying either the initial default value (line 35)
+          or the value they typed in (line 84)
 
-          
+          NOTE this line:
 
-          
+          <?php if($emailERROR) print 'class="mistake"'; ?>
 
-          
-          
+          this prints out a css class so that we can highlight the background etc. to
+          make it stand out that a mistake happened here.
 
          */
         ?>
@@ -291,11 +290,12 @@ if (isset($_POST["btnSubmit"])) {
                                    
                                    
                         
+                        
                         <label for="txtEmail" class="required">Email
                             <input type="text" id="txtEmail" name="txtEmail"
                                    value=""
                                    tabindex="120" maxlength="45" placeholder="Enter a valid email address"
-                                   
+                                   <?php if ($emailERROR) print 'class="mistake"'; ?>
                                    onfocus="this.select()" 
                                    autofocus>
                         </label>
@@ -312,7 +312,7 @@ if (isset($_POST["btnSubmit"])) {
         </form>
 
     <?php
-     
+    } // end body submit
     ?>
 
  </article>
